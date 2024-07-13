c1548441e152995825a8b1fa1fc4b143d20955f3
Patric Borges <patric.costaborges@hoymail.com>
2024-07-13 17:35:31 -0300

    calculator component, set globals css and test.

---
+++ src/app/Calculator.tsx
@@ -0,0 +1,8 @@
+export default function Calculator() {
+  return (
+    <div className="h-80 w-60 rounded-md overflow-hidden bg-red-500">
+      <h1>Calculator</h1>
+      <p>TODO: Implement calculator here</p>
+    </div>
+  );
+}
